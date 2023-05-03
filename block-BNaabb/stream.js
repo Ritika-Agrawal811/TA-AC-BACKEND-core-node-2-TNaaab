b7e1a0a87b94f4fdac356372b5ba4b330cf34211
Ritika Agrawal <ritikaagrawal339@gmail.com>
2023-05-03 16:30:34 +0530

    complete: Assignment Fetch Request Body

--- block-BNaabb/stream.js
+++ block-BNaabb/stream.js
@@ -1,1 +1,20 @@
-// Setup
+const http = require("http");
+const port = 3456;
+const host = "localhost";
+
+const server = http.createServer((req, res) => {
+  let store = "";
+
+  req.on("data", (chunk) => {
+    store += chunk;
+  });
+
+  req.on("end", () => {
+    res.write(store);
+    res.end();
+  });
+});
+
+server.listen(port, host, () => {
+  console.log("server started");
+});
