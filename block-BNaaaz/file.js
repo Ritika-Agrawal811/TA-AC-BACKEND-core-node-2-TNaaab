eb579d8d6d3782660a14dd89515aa0c1f6a9195b
Ritika Agrawal <ritikaagrawal339@gmail.com>
2023-05-03 15:59:46 +0530

    complete: Assignment FS Module

--- block-BNaaaz/file.js
+++ block-BNaaaz/file.js
@@ -1,1 +1,13 @@
-// Setup
+const http = require("http");
+const fs = require("fs");
+
+const host = "localhost";
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+  fs.createReadStream("./readme.txt").pipe(res);
+});
+
+server.listen(port, host, () => {
+  console.log("sever started");
+});
