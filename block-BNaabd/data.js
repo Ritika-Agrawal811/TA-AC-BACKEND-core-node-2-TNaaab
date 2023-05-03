44e1052e23220b3dcd8977c7fe248d7dcbb11246
Ritika Agrawal <ritikaagrawal339@gmail.com>
2023-05-03 17:08:50 +0530

    complete: Assignment Parsing Request Data

--- block-BNaabd/data.js
+++ block-BNaabd/data.js
@@ -1,1 +1,26 @@
-// Setup
+const http = require("http");
+const qs = require("querystring");
+const port = 7000;
+const host = "localhost";
+
+const server = http.createServer((req, res) => {
+  const dataFormat = req.headers["content-type"];
+  let store = "";
+
+  req.on("data", (chunk) => {
+    store += chunk;
+  });
+
+  req.on("end", () => {
+    if (dataFormat == "application/json") {
+      res.end(store);
+    } else if (dataFormat == "application/x-www-form-urlencoded") {
+      const parsedData = qs.parse(store);
+      res.end(JSON.stringify(parsedData));
+    }
+  });
+});
+
+server.listen(port, host, () => {
+  console.log("server started");
+});
